96886cdb1476b500aa05b8788e5a76d96e3c254a
Solveigh <solveigh19@googlemail.com>
2017-09-17 15:50:31 +0200

    parameters os still image script updated

--- exciss_python_master/shell/run_still.sh
+++ exciss_python_master/shell/run_still.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # test script to launch the mission_cam python module
-python /home/pi/EXCISS/exciss_python_master/python/mission_cam_cmd.py -c "raspistill -md 4 -w 1640 -h 1232 -fps 40 -ex fixedfps -awb auto --segment 1000 -v -o DATE_TIME_40fps_seg%02d.jpg 2>&1"
+python /home/pi/EXCISS/exciss_python_master/python/mission_cam_cmd.py -c "raspistill -md 4 -w 1640 -h 1232 -awb auto -v -o DATE_TIME_40fps_seg%02d.jpg 2>&1"
