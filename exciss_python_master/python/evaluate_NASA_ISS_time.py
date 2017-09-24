16cd5ebd6d06239fadfa96c3082c9516091b4504
Solveigh <solveigh19@googlemail.com>
2017-09-24 16:22:03 +0200

    extension of python script to read time and date file: send set or restore via  serial connection; quit if time and date file is not available

--- exciss_python_master/python/evaluate_NASA_ISS_time.py
+++ exciss_python_master/python/evaluate_NASA_ISS_time.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# python module to read the NASA ISS time file
+# Python module to read the NASA ISS time file
+# and set or restore the time of the RTC via serial connection.
+#
+# Nothing happens if the file is not available
 #
 # Author: Solveigh
 #
 # run:
-# python evaluate_NASA_ISS_time.py -p "/home/solveigh/Desktop/exciss/EXCISS/contributors/solveigh/NASA_ISS_TIME_DUMMY_FILE"
+# python evaluate_NASA_ISS_time.py -f "/home/solveigh/Desktop/exciss/EXCISS/contributors/solveigh/NASA_ISS_TIME_DUMMY_FILE" -m "set/restore" -s "/dev/ttyAMA0" -b 9600
 
 import sys, getopt
 import logging
 import os
+import serial
 
 #########################LOGGING##########################
 # log file name is identical to the script name
@@ -38,48 +42,114 @@ def readFile(fileName):
     print(str(time_string))
     logger.info('Content of ' + fileName + ': ' + time_string)
 
-    '''
-    # split time and date string
-    splitted_time = time_string.split('_')
-    if len(splitted_time) == 6:
-      print('Y ' + splitted_time[0])
-      print('M ' + splitted_time[1])
-      print('D ' + splitted_time[2])
-      print('h ' + splitted_time[3])
-      print('m ' + splitted_time[4])
-      print('s ' + splitted_time[5])
-    '''
     return time_string
-  except OSError: # py2: FileNotFoundError:
+  except IOError:
+    logger.error('Error reading file: ' + fileName)
+    return ""
+  except OSError:
     logger.error('Error reading file: ' + fileName)
     return ""
   return ""
 #########################FILE#############################
 
-#########################MAIN##########################
+#########################SERIAL###########################
+serialPort = '/dev/ttyAMA0'
+baudRate = 9600
+
+def sendSerialCommand(cmd):
+  global serialPort
+  global baudRate
+
+  ser = None
+
+  try:
+    logger.info("Start serial communication.")
+
+    # configure the serial connections (the parameters differ on the device you are connecting to)
+    ser = serial.Serial(
+      port=serialPort,
+      baudrate=baudRate
+    )
+
+    ser.close() # close port in case it is still open
+
+    ser.open()
+
+    if ser.isOpen():
+      logger.info("Serial port open.")
+    else:
+      logger.error("Serial port not open")
+
+    logger.info("Serial port: " + ser.name)
+
+    # send the character to the device
+    # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+    logger.info("Sending to serial port: " + str(cmd) + '\r\n')
+    ser.write(str(cmd) + '\r\n')
+
+    out = ''
+    # let's wait one second before reading output (let's give device time to answer)
+    time.sleep(1)
+    while ser.inWaiting() > 0:
+      out += str(ser.read(1))
+      if out != '':
+        if '\r\n' in out:
+          print out
+
+  except serial.SerialException, e:
+    logger.error("An exception occured: " + str(e))
+  finally:
+    logger.info("Closing serial port.")
+    if ser != None:
+      ser.close()
+
+  logger.info("End serial communication.")
+#########################SERIAL###########################
+
+#########################MAIN#############################
 def main(argv):
   print 'Number of arguments:', len(sys.argv), 'arguments.'
   print 'Argument List:', str(sys.argv)
 
-  parameter1 = 0
+  fileName = 0
+  mode = 0
   try:
     logger.info('Reading command line parameters')
-    opts, args = getopt.getopt(argv,"hp:",["parameter1="])
+    opts, args = getopt.getopt(argv,"hf:m:s:b:",["filename=", "mode=", "serialPort=", "baudRate="])
   except getopt.GetoptError:
-    print 'python ' + str(sys.argv[0]) + ' -p <parameter1>'
-    logger.error('python ' + str(sys.argv[0]) + ' -p <parameter1>')
+    print 'python ' + str(sys.argv[0]) + ' -f <filename> -m <mode> -s <serialPort> -b <baudRate>'
+    logger.error('python ' + str(sys.argv[0]) + ' -f <filename> -m <mode> -s <serialPort> -b <baudRate>')
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print str(sys.argv[0]) + ' -p <parameter1>'
+      print str(sys.argv[0]) + ' -f <filename> -m <mode> -s <serialPort> -b <baudRate>'
       sys.exit()
-    elif opt in ("-p", "--parameter1"):
-      parameter1 = str(arg)
+    elif opt in ("-f", "--filename"):
+      filename = str(arg)
+    elif opt in ("-m", "--mode"):
+      mode = str(arg)
+    elif opt in ("-s", "--serialPort"):
+      serialPort = str(arg)
+    elif opt in ("-b", "--baudRate"):
+      baudRate = int(arg)
 
-  print 'Parameter 1 is: ' + str(parameter1)
-  logger.info('Parameter 1 is: ' + str(parameter1))
-
-  readFile(parameter1)
+  print 'Filename is: ' + str(filename)
+  logger.info('Filename is: ' + str(filename))
+  print 'Mode is: ' + str(mode)
+  logger.info('Mode is: ' + str(mode))
+  
+  time_string = readFile(filename)
+  
+  if time_string != "":
+    if mode == "set":
+      logger.info('Set time and date')
+      sendSerialCommand("wtd" + time_string)
+    if mode == "restore":
+      logger.info('Restore time and date')
+      sendSerialCommand("wtr" + time_string)
+  else:
+    print('Nothing done, no time and date file available: ' + time_string)
+    logger.info('Nothing done, no time and date file available: ' + time_string)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
